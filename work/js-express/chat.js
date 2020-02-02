6c6e0e58aa717bc283f48c9f3505664856805216
Sahithi Sarabu <sahithi.sarabu@gmail.com>
2020-02-02 00:07:17 -0800

    edit and add chat.js and chat-web.js

--- work/js-express/chat.js
+++ work/js-express/chat.js
@@ -1,1 +1,18 @@
-module.exports = {};
+const chat = {
+	users: {
+	},
+
+	messages: [
+	],
+
+	addMessage: function({text, sender, timestamp= new Date()}){
+		if(!chat.users[sender]){
+			chat.users[sender] = sender;
+		}
+		chat.messages.push({text,sender,timestamp});
+	},
+
+};
+
+
+module.exports = chat;
